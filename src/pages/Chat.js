f737f2e9ed3d494d230478a72a24182118dab238
Gnaig <tangiang0812@gmail.com>
2022-05-24 21:40:53 +0700

    basic chat page

--- src/pages/Chat.js
+++ src/pages/Chat.js
@@ -1,7 +1,21 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import SideBar from "../components/Sidebar";
+import MessageForm from "../components/MessageForm";
 
 function Chat() {
-  return <div>Chat</div>;
+  return (
+    <Container>
+      <Row>
+        <Col md={4}>
+          <SideBar></SideBar>
+        </Col>
+        <Col md={8}>
+          <MessageForm></MessageForm>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Chat;
