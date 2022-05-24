9c78426dd1ef66b610237d09f31612ff60eb57e9
Gnaig <tangiang0812@gmail.com>
2022-05-24 17:41:22 +0700

    styled login page and signup page

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,7 +1,47 @@
 import React from "react";
+import { Button, Card, Col, Container, Form, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function Signup() {
-  return <div>Signup</div>;
+  return (
+    <Container>
+      <Row>
+        <Col className="offset-3 col-6">
+          <Card style={{ width: "100%" }} className="mt-3">
+            <Card.Body>
+              <Form>
+                <Form.Group className="mb-3" controlId="formBasicName">
+                  <Form.Label>Your Name</Form.Label>
+                  <Form.Control type="text" placeholder="Enter username" />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                  <Form.Label>Email address</Form.Label>
+                  <Form.Control type="email" placeholder="Enter email" />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                  <Form.Label>Password</Form.Label>
+                  <Form.Control type="password" placeholder="Password" />
+                </Form.Group>
+                {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                  <Form.Check type="checkbox" label="Check me out" />
+                </Form.Group> */}
+                <Button variant="primary" type="submit">
+                  Submit
+                </Button>
+                <div className="mt-3">
+                  <p className="text-center">
+                    {" "}
+                    Already have an account ? <Link to="/login">Login</Link>
+                  </p>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default Signup;
